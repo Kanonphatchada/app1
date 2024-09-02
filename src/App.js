162d362ce40f52961bea53e38c1ec4e271482183
Kanonphatchada <hunter09032546@gmail.com>
2024-09-02 11:25:59 +0700

    context-content2.js
    context-content2.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { userContext } from './context';
 import Header2 from './context-header2';
+import Content2  from './context-content2';
 
 
 export default function App() {
@@ -9,7 +10,8 @@ export default function App() {
     <userContext.Provider value={[user,setUser]}>
 
       <Header2/>
-
+      <Content2/>
+      
     </userContext.Provider>
   )
 }
