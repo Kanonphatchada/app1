cf442368e068423cf0b4390044fe5dea96667cd7
Kanonphatchada <hunter09032546@gmail.com>
2024-09-30 12:08:12 +0700

    form-select-file.js
    form-select-file.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from "react"
-import Router3 from "./router3"
-function App() {
-  return (
-    <>
-      <Router3 />
+import FromSelectFile from "./form-select-file"
 
-    </>
-  )
+export default function App() {
+  return <FromSelectFile/>
 }
-export default App
